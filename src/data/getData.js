9d9f8a202ccf503e41507787a274db78bfb1a4eb
E190989X <mathieu.murie@etu.univ-nantes.fr>
2022-05-13 10:40:03 +0200

    feat : json data & API request

---
+++ src/data/getData.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Departements from "./departements.json";
+
+const randomProperty = function (obj) {
+    var keys = Object.keys(obj);
+    return obj[keys[keys.length * Math.random() << 0]];
+};
+
+const getCommunes = async () => {
+    let randomDep = randomProperty(Departements);
+
+    const response = await fetch("https://geo.api.gouv.fr/departements/" + randomDep["code"] + "/communes?fields=nom,centre,departement,population&format=json&geometry=centre");
+    const jsonData = await response.json();
+
+    let randomCommune = randomProperty(jsonData);
+    console.log(randomCommune);
+
+};
+export default getCommunes;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import CarteFrance from '../components/CarteFrance';
+
+const Home = () => {
+    return (
+        <div>
+            <CarteFrance></CarteFrance>
+        </div>
+    )
+};
+
+export default Home;
