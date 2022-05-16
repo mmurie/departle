4cad319628019661a4e2a66263b08f9a9ad2595f
E190989X <mathieu.murie@etu.univ-nantes.fr>
2022-05-16 07:58:36 +0200

    feat : WIP navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import { useContext } from 'react';
+import { NavLink } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div id="home" class="container">
             <h1 class="title">DEPARTLE</h1>
+            <ul class="liste-boutons">
+                <NavLink to="/classique" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+                    <li>Mode classique</li>
+                </NavLink>
+            </ul>
         </div>
     )
 };
