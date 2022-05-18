fc5a9c3244b69cb2d1f6f2128109247b789fed0d
E190989X <mathieu.murie@etu.univ-nantes.fr>
2022-05-18 11:11:51 +0200

    feat : routes mode carte / mode forme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import AutoComplete from './pages/AutoComplete';
-import fetchCommunes from './data/getData';
 import ClassicGame from './pages/ClassicGame';
 import ModeClassique from './pages/ModeClassique';
+import ModeCarte from './pages/ModeCarte';
+import ModeForme from './pages/ModeForme';
 import Page404 from './pages/404';
 
 const App = () => {
@@ -11,10 +12,11 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}> </Route>
-        <Route path="*" element={<Home />}> </Route> {/*404*/}
         <Route path="/test" element={<AutoComplete />}> </Route>
         <Route path="/classic" element={<ClassicGame />}> </Route>
         <Route path="/classique" element={<ModeClassique />}> </Route>
+        <Route path="/carte" element={<ModeCarte />}> </Route>
+        <Route path="/forme" element={<ModeForme />}> </Route>
         <Route path="*" element={<Page404 />}> </Route> {/*404*/}
       </Routes>
     </BrowserRouter>
